4355e95204e15f8bd2b69c8279a52557959ef842
LeonardTarigan <leontrg32@gmail.com>
2023-12-27 23:10:37 +0700

    slicing navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue'
+</script>
 
 <template>
+  <NavBar />
   <main>Blibli Test</main>
 </template>
+
+<style>
+@import './assets/main.css';
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="nav">
+    <a href="/">
+      <img src="../assets/blibli-logo.png" alt="Logo" />
+    </a>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  background-color: var(--blibli-blue);
+  padding: 1rem 3rem;
+  color: white;
+  display: flex;
+  justify-content: center;
+}
+
+@media (min-width: 640px) {
+  nav {
+    justify-content: start;
+  }
+}
+</style>
